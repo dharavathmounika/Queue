69e683930d8fc8d0f24229f3d66c23e708ce8e9d
mounika <monikadharavath7@gmail.com>
2022-02-13 19:25:12 +0530

    phase 1

---
+++ Queue/src/QueueExample1.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+ class QueueExample1 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Queue<String> locationsQueue = new LinkedList<>();
+		locationsQueue.add("xyz");
+		        		locationsQueue.add("abc");
+		        		locationsQueue.add("def");
+		        		locationsQueue.add("ghi");
+		        		locationsQueue.add("jkl");
+		System.out.println("Queue is : " + locationsQueue);
+		        		System.out.println("Head of Queue : " + locationsQueue.peek());
+		        		locationsQueue.remove();
+		        		System.out.println("After removing Head of Queue : " + locationsQueue);
+		        		System.out.println("Size of Queue : " + locationsQueue.size());
+		    	}
+
+	}
+
